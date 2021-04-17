028da4f571803527bfa05c8cdad8779aff84d816
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-04-17 15:00:30 -0400

    Add copyright statements to each file

--- server/lib/index.ts
+++ server/lib/index.ts
@@ -1,2 +1,6 @@
+/*
+ * Copyright 2021 Phillip Gates-Shannon. All rights reserved. Licensed under the Open Software License version 3.0.
+ */
+
 export { DbClient, PgClient, db, pg } from './database-client';
 export * from './message-store';
