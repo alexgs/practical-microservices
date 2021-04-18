55912488ca09997a3e87efe001c6e17e595963ea
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-04-18 12:44:30 -0400

    Refactor test

--- server/src/message-store/create-subscription-factory.test.ts
+++ server/src/message-store/create-subscription-factory.test.ts
@@ -1,26 +1,47 @@
-import { createSubscriptionFactory } from './create-subscription-factory';
+import {
+  CreateSubscriptionOptions,
+  createSubscriptionFactory,
+} from './create-subscription-factory';
+
+interface MockFactoryConfig {
+  read: jest.Mock;
+  readLastMessage: jest.Mock;
+  write: jest.Mock;
+}
+
+function getConfig(override?: MockFactoryConfig): MockFactoryConfig {
+  return {
+    read: jest.fn(),
+    readLastMessage: jest.fn(),
+    write: jest.fn(),
+    ...override,
+  };
+}
+
+function getOptions(
+  override?: CreateSubscriptionOptions,
+): CreateSubscriptionOptions {
+  return {
+    handlers: {},
+    streamName: 'testing',
+    subscriberId: 'tests:create-subscription-factory',
+    ...override,
+  };
+}
 
 describe('The `Subscription` object', () => {
   describe('The `savePosition` function', () => {
     it('calls the message writer', async () => {
-      const mocks = {
-        read: jest.fn(),
-        readLastMessage: jest.fn(),
-        write: jest.fn(),
-      };
-      const createSubscription = createSubscriptionFactory(mocks);
-      const options = {
-        handlers: {},
-        streamName: 'testing',
-        subscriberId: 'tests:create-subscription-factory',
-      };
+      const config = getConfig();
+      const options = getOptions();
+      const createSubscription = createSubscriptionFactory(config);
       const subscription = createSubscription(options);
 
       const position = 10;
       await subscription._savePosition(position);
-      expect(mocks.write).toHaveBeenCalledTimes(1);
+      expect(config.write).toHaveBeenCalledTimes(1);
 
-      const args = mocks.write.mock.calls[0];
+      const args = config.write.mock.calls[0];
       expect(args[0]).toEqual(`subscriberPosition-${options.subscriberId}`);
       expect(args[1]).toMatchObject({
         id: expect.any(String),
