8988c88ba9aaca96d650efbf3652210b651cd272
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-04-18 14:44:16 -0400

    Reorganize `reader-factory` files

--- server/src/message-store/writer-factory.ts
+++ server/src/message-store/writer-factory.ts
@@ -15,6 +15,7 @@ type WriteValues = [string, string, string, JsonB, JsonB | null, number | null];
 const SQL_WRITE_FN = 'SELECT write_message($1, $2, $3, $4, $5, $6)';
 
 export function writerFactory(db: MessageDatabase): WriteFn {
+  // TODO Refactor this so there's a `writer` object, so the syntax here parallels what's in `reader-factory`
   return async function write(
     streamName: string,
     message: EventInput,
